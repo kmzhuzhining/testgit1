e447b14dd3c6702225f60d1d82a67345b4a9491d
zzn <zzn@DESKTOP-FKEUGC6>
2018-03-04 17:09:23 +0800

    no1

---
+++ src/testgit1/TestGit1.java
@@ -0,0 +1,12 @@
+package testgit1;
+
+public class TestGit1 {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+	}
+
+}
